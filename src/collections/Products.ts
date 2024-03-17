c50c3a94db43ed1aad23dc324e775bbdc3d1f47f
mendoza000 <omarmendoza.alv25@gmail.com>
2024-03-17 12:49:18 -0400

    upload project

---
+++ src/collections/Products.ts
@@ -0,0 +1,37 @@
+import { CollectionConfig } from 'payload/types'
+
+const Products: CollectionConfig = {
+  slug: 'products',
+  admin: {
+    useAsTitle: 'name',
+  },
+  fields: [
+    {
+      name: 'name',
+      label: 'Name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      label: 'Description',
+      type: 'textarea',
+      required: true,
+    },
+    {
+      name: 'price',
+      label: 'Price',
+      type: 'number',
+      required: true,
+    },
+    {
+      name: 'image',
+      label: 'Image',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+    }
+  ],
+}
+
+export default Products
---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import express from 'express'
+import payload from 'payload'
+import { mediaManagement } from "payload-cloudinary-plugin";
+
+require('dotenv').config()
+const app = express()
+
+// Redirect root to Admin panel
+app.get('/', (_, res) => {
+  res.redirect('/admin')
+})
+
+app.use(mediaManagement(
+  {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  }
+))
+
+const start = async () => {
+  // Initialize Payload
+  await payload.init({
+    secret: process.env.PAYLOAD_SECRET,
+    express: app,
+    onInit: async () => {
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+    },
+  })
+
+  // Add your own express routes here
+
+  app.listen(3000)
+}
+
+start()
